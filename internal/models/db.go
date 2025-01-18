638d6c17f4bcbbd941c0750e422433ed1fc4a41d
gantoho <i@ganto.me>
2025-01-18 22:27:34 +0800

    perf: 优化

--- internal/models/db.go
+++ internal/models/db.go
@@ -15,8 +15,8 @@ func InitDB() {
 	password := readLocalPassword() //密码
 	host := "127.0.0.1"             //数据库地址，可以是Ip或者域名
 	port := 3306                    //数据库端口
-	Dbname := "osys"                //数据库名
-	dsn := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8&parseTime=True&loc=Local", username, password, host, port, Dbname)
+	dbname := "osys"                //数据库名
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8&parseTime=True&loc=Local", username, password, host, port, dbname)
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		panic("连接数据库失败, error=" + err.Error())
